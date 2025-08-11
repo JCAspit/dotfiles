df5af6bee986359fd7c2f252d971e4217640403e
JCAspit <wr2412b@gmail.com>
2025-08-10 23:30:10 -0400

    moved scripts to separate repo

--- scripts/volume.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ $(pactl get-sink-mute $(pactl get-default-sink) | cut -d ' ' -f 2) = "yes" ]]; then
-    notify-send -t 2000 -r 2593 -u normal "Volume: Mute"
-else
-	volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+(?=%)' | sed '$d')
-    notify-send -t 2000 -r 2593 -u normal -h int:value:"$volume" "Volume: ${volume}%"
-fi
