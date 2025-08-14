fb45d1b234150292af5d8a9f7f5f3bc75c924efd
JCAspit <wr2412b@gmail.com>
2025-08-14 02:44:41 -0400

    updated nvim telescope keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,15 @@
+local vim = vim
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', "<leader>pf", builtin.find_files, {})
-vim.keymap.set('n', "<C-p>", builtin.git_files, {})
+--vim.keymap.set('n', "<C-p>", builtin.git_files, {})
 vim.keymap.set('n', "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set('n', "<leader>fh", function()
+	require('telescope.builtin').help_tags()
+end)
+
+--vim.keymap.set('n', "<leader>fg", function()
+--	require('telescope.builtin').live_grep()
+--end)
